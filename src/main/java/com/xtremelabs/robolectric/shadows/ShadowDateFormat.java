2341416ebec638f06f8e1a5b0cb3172e1ae92d5d
Christian Williams <xian@squareup.com>
2013-01-22 14:09:32 -0800

    Merge library resource loaders (partially finished). Easier to override finding of libraries.

--- src/main/java/com/xtremelabs/robolectric/shadows/ShadowDateFormat.java
+++ src/main/java/com/xtremelabs/robolectric/shadows/ShadowDateFormat.java
@@ -11,13 +11,18 @@ import java.util.Locale;
 @Implements(DateFormat.class)
 public class ShadowDateFormat {
 
-	@Implementation
-	public static java.text.DateFormat getDateFormat(Context context) {
-        return new java.text.SimpleDateFormat("MMM-DD-yyyy", Locale.ROOT);
-	}
+    @Implementation
+    public static java.text.DateFormat getLongDateFormat(android.content.Context context) {
+        return new java.text.SimpleDateFormat("MMMMM DD, yyyy", Locale.ROOT);
+    }
 
-	@Implementation
-	public static java.text.DateFormat getTimeFormat(Context context) {
+    @Implementation
+    public static java.text.DateFormat getDateFormat(Context context) {
+        return new java.text.SimpleDateFormat("MMM-DD-yyyy", Locale.ROOT);
+    }
+
+    @Implementation
+    public static java.text.DateFormat getTimeFormat(Context context) {
         return new java.text.SimpleDateFormat("HH:mm:ss", Locale.ROOT);
-	}
+    }
 }
